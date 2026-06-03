c01fd5865ff598c8e323ae226e18b763b9d45aeb
Martin <martin@awadlouis.com>
2026-06-03 13:53:12 +0200

    Fix cli entry point

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="os-disk-scheduling",
     version="0.1.0",
     py_modules=["simulator"],
+    packages=["src"],
     entry_points={
         "console_scripts": [
             "run-simulator=simulator:main",
